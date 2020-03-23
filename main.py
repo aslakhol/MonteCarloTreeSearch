37ff77824e3c4c2174b5d9cf9f0b93c939387b62
Aslak Hollund <aslakhol@gmail.com>
2020-03-23 22:53:22 +0100

    some random traversal

--- main.py
+++ main.py
@@ -9,10 +9,24 @@ tree = MonteCarloSearchTree()
 root_node = MonteCarloSearchNode(True, children=game.generate_child_states())
 
 
-print(root_node)
+def traverse_randomly(node):
+    print(node)
+    print("traversal step")
+    if len(node.children) <= 1:
+        print("The end")
+        return
+
+    random_child = MCSN_from_game(random.choice(node.children))
+    print(random_child)
+    return traverse_randomly(random_child)
+
+
+def MCSN_from_game(game_object):
+    print(game_object.get_state())
+    return MonteCarloSearchNode(False, game_object.generate_child_states())
+
+
 # while not game.is_end_state():
 #     tree.suggest_action()
 
-# while not game.is_end_state():
-#     legal_moves = game.get_legal_moves()
-#     game.move(random.choice(legal_moves))
+traverse_randomly(root_node)
