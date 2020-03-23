96e0a855a75ded58842ad4fb96ac1f9b6f8f4d44
s0lvang <august.s.solvang@gmail.com>
2020-03-23 16:09:48 +0100

    adds config file

---
+++ config.py
@@ -0,0 +1,18 @@
+nim = dict(
+    number_in_batch=100,
+    starting_player=1,
+    number_of_stones=100,
+    max_take=12
+)
+
+ledge = dict(
+    number_in_batch=100,
+    initial_board="10000210001000",
+    starting_player=1,
+    number_of_simulations=12
+)
+
+general = dict(
+    verbose_mode=True,
+    win_statistics_batch=True,
+)
